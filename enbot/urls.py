ffdb70ae691d76ff3eba5f7981b024a43d39bb64
vp37 <vigneshprabutbvsh@gmail.com>
2025-08-19 20:08:47 +0530

    completed

--- enbot/urls.py
+++ enbot/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.http import JsonResponse
 from chatai.views import FinanceChatBotView
 
+# Root view
+def index(request):
+    return JsonResponse({"message": "Enbot backend is running!"})
+
 urlpatterns = [
+    path("", index),  # root path
     path("chat/", FinanceChatBotView.as_view(), name="FinanceChatBotView"),
 ]
+
 
 
