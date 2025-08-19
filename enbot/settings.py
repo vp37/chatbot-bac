a56f24445dd62c27e091d6993a8a66fd97131741
vp37 <vigneshprabutbvsh@gmail.com>
2025-08-19 19:55:17 +0530

    completed

--- enbot/settings.py
+++ enbot/settings.py
@@ -135,4 +135,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://chatbot-fro.vercel.app",  # NO trailing slash
 ]
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
